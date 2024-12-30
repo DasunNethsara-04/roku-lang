d78c15803cfaefcd204791e163175f9aae0523f5
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-30 14:21:44 +0530

    Enhance Util class with detailed docstrings for cast_num_type and colorize_text methods

--- util.py
+++ util.py
@@ -6,6 +6,18 @@ class Util:
         pass
     
     def cast_num_type(self, num: str|Any) -> int|float|ValueError:
+        """
+        Casts a given input to either an integer or a float, and raises a ValueError if it's not a number.
+        
+        Args:
+            num (str | Any): Get a number to cast
+
+        Raises:
+            ValueError: If the `num` is not in a valid type
+
+        Returns:
+            int|float|ValueError: casted value or a error
+        """
         try:
             return int(num)
         except ValueError:
@@ -14,7 +26,19 @@ class Util:
             except ValueError:
                 raise ValueError(f"Invalid input: '{num}' is not a number.")
             
-    def colorize_text(self, type: str, message):
+    def colorize_text(self, type: str, message: str) -> str:
+        """Colorizes the given message based on the specified type.
+
+        Args:
+            type (str): Type of the message
+                            > error
+                            
+                            > warning
+            message (str): The message that needs to colorize
+
+        Returns:
+            str: Colorized ASCII string
+        """
         if type == "success":
             return f"{colorama.Fore.GREEN}{message}{colorama.Style.RESET_ALL}"
         elif type == "warning":
