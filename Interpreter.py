612a4326fee8673e146529f07a32db700a183f69
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-12-30 14:25:29 +0530

    Refactor error handling in Interpreter class to streamline arithmetic operation execution

--- Interpreter.py
+++ Interpreter.py
@@ -30,40 +30,28 @@ class Interpreter:
         """
         Parses the given token list and performs the corresponding action.
         """
-        if len(token) < 1:
-            pass
-        elif token[0] == "PRINT":
-            print((" ").join(token[1:]))
-        elif token[0] == "ADD":
-            try:
+        try:
+            if len(token) < 1:
+                pass
+            elif token[0] == "PRINT":
+                print((" ").join(token[1:]))
+            elif token[0] == "ADD":
                 result = self.armt.add(token[1:])
                 print(result)
-            except TypeError as e:
-                print(self.util.colorize_text("error", f"Error: {str(e)}"))
-        elif token[0] == "SUB":
-            try:
+            elif token[0] == "SUB":
                 result = self.armt.subtract(token[1:])
                 print(result)
-            except TypeError as e:
-                print(self.util.colorize_text("error", f"Error: {str(e)}"))
-        elif token[0] == "MUL":
-            try:
+            elif token[0] == "MUL":
                 result = self.armt.multiply(token[1:])
                 print(result)
-            except TypeError as e:
-                print(self.util.colorize_text("error", f"Error: {str(e)}"))
-        elif token[0] == "DIV":
-            try:
+            elif token[0] == "DIV":
                 result = self.armt.divide(token[1], token[2])
                 print(result)
-            except TypeError as e:
-                print(self.util.colorize_text("error", f"Error: {str(e)}"))
-        elif token[0] == "MOD":
-            try:
+            elif token[0] == "MOD":
                 result = self.armt.modulus(token[1], token[2])
                 print(result)
-            except TypeError as e:
-                print(self.util.colorize_text("error", f"Error: {str(e)}"))
+        except Exception as e:
+            print(self.util.colorize_text("error", f"Error: {str(e)}"))
             
     
     def execute(self) -> None:
