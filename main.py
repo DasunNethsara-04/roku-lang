fa5fbf9bace0c70460952efff36e1f2cd5877a3b
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-01-01 23:19:52 +0530

    Refactor main.py to use CodeExecutor for executing content and add error handling for file operations

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys
-from Interpreter import Interpreter
+import Executor
 
-if __name__ == "__main__":
-    content = open(sys.argv[1]).read()
-    interpreter: Interpreter = Interpreter(content)
-    interpreter.execute()
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        try:
+            content: str = open(sys.argv[1]).read()
+        except FileNotFoundError | IndexError as e:
+            print("File not found.\nError: ", e)
+            sys.exit()
+        finally:
+            Executor.CodeExecutor(content)
