e56c18aab4df8f0cc761b57cccc4f2776d521681
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-01-01 23:29:01 +0530

    Use pprint for better formatting of tokenized data in CodeExecutor

--- Executor.py
+++ Executor.py
@@ -1,5 +1,6 @@
+import pprint
 import Lexer
 class CodeExecutor:
     def __init__(self, content: str) -> None:
         data: list[dict] = Lexer.Lexer(content).tokenizer()
-        print(data)
+        pprint.pprint(data)
